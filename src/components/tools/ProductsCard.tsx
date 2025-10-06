8c3fec1ef39f289e5702062572b7b6a2ddc986d6
Wharton Aldrick (xeon-sanctuary) <whartonaldrick@gmail.com>
2025-10-07 01:18:24 +0300

    Improve UI : Products Card

--- src/components/tools/ProductsCard.tsx
+++ src/components/tools/ProductsCard.tsx
@@ -7,23 +7,27 @@ interface ProductsCardProps {
 
 const ProductsCard: React.FC<ProductsCardProps> = ({ product }) => {
     return (
-        <div className='border border-accent/30 w-60 rounded-xl'>
-            <figure>
-                <img
-                    src={ product.image_url || undefined}
-                    alt={ product.title || ""}
-                    className='w-full p-2'
-                />
-            </figure>
-            <div>
-                <h2>
-                    {product?.title}
-                </h2>
-                <p>
-                    {product?.description}
-                </p>
-                <div className='badge badge-accent badge-outline'>
-                    {product?.type}
+        <div className='border border-accent/30 w-70 mx-auto rounded-xl overflow-clip bg-base-300'>
+            <div className='bg-base-300'>
+                <figure className='rounded-br-2xl bg-white'>
+                    <img
+                        src={product.image_url || undefined}
+                        alt={product.title || ""}
+                        className='w-3/4 mx-auto'
+                    />
+                </figure>
+            </div>
+            <div className='bg-white h-max'>
+                <div className='px-5 bg-base-300 flex flex-col py-5 rounded-tl-2xl'>
+                    <div className='text-xs uppercase'>
+                        {product?.type}
+                    </div>
+                    <h2 className='font-extrabold text-lg'>
+                        {product?.title}
+                    </h2>
+                    <p className='text-sm'>
+                        {product?.description}
+                    </p>
                 </div>
             </div>
         </div>
