2b03991905d57a13c613b49585d4aaace23b6153
Wharton Aldrick (xeon-sanctuary) <whartonaldrick@gmail.com>
2025-10-13 20:31:31 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/modules/Hero.tsx
+++ src/components/modules/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import heroImg from './../../assets/images/heroImage.png'
+import heroImg from './../../assets/images/carte.jpg'
 
 const Hero: React.FC = () => {
     return (
